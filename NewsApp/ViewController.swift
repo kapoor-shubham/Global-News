b1796caef3c3fbf1e57f8f96e031a99ac24665fd
Shubham Kapoor <Shubham.Kapoor@arris.com>
2020-02-10 14:20:48 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Shubham Kapoor on 10/02/20.
+//  Copyright © 2020 Shubham Kapoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
